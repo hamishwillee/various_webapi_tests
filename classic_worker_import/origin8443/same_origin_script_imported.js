144b9989cac64f9ac78052a9522462d6cc969cb4
Hamish Willee <hamishwillee@gmail.com>
2025-12-16 14:43:32 +1100

    Add classic_worker_import tests

---
+++ classic_worker_import/origin8443/same_origin_script_imported.js
@@ -0,0 +1,2 @@
+console.log("IMPORTSCRIPT (S0): same_origin_script_imported: run");
+self.postMessage("IMPORTSCRIPT (S0): same_origin_script_imported: run");
---
+++ classic_worker_import/origin9443/cross_origin_script_imported.js
@@ -0,0 +1,2 @@
+console.log("IMPORTSCRIPT (C0): cross_origin_script_imported: run");
+self.postMessage("IMPORTSCRIPT (C0): cross_origin_script_imported: run");
